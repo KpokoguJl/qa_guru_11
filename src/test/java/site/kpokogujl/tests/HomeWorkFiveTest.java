44642d9402bb0be1d92ebaf946d07c1e57db7d6c
varlamova <varlamvanadia@mail.ru>
2022-02-11 16:33:31 +0300

    Сделала рандом для переменной телефона

--- src/test/java/site/kpokogujl/tests/HomeWorkFiveTest.java
+++ src/test/java/site/kpokogujl/tests/HomeWorkFiveTest.java
@@ -19,7 +19,7 @@ public class HomeWorkFiveTest {
         String firstName = faker.name().firstName(),
                 lastName = faker.name().lastName(),
                 email = faker.internet().emailAddress(),
-                phone = "9121122233",
+                phone = faker.phoneNumber().subscriberNumber(10),
                 address = faker.address().fullAddress(),
                 gender = "Female",
                 day = "10",
